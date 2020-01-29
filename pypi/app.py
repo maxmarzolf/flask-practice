3935b4cef41baafa166342bf53311a28557c46e8
maxmarzolf <max@treesfromspace.com>
2020-01-28 21:01:26 -0500

    decorator for rendered templates, reorganized template file structure

--- pypi/app.py
+++ pypi/app.py
@@ -1,4 +1,11 @@
+import os
+import sys
 import flask
+folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, folder)
+
+
+from pypi.infrastructure.view_modifiers import response
 
 app = flask.Flask(__name__)
 
@@ -6,20 +13,23 @@ app = flask.Flask(__name__)
 def get_latest_packages():
     return [
         {'name': 'flask', 'version': '1.2.3'},
-        {'name': 'passlib', 'version': '1.2.3'},
-        {'name': 'sqlalchemy', 'version': '1.2.3'},
+        {'name': 'sqlalchemy', 'version': '2.2.0'},
+        {'name': 'passlib', 'version': '3.0.0'},
     ]
 
 
 @app.route('/')
+@response(template_file='home/index.html')
 def index():
     test_packages = get_latest_packages()
-    return flask.render_template('index.html', packages=test_packages)
+    return {'packages': test_packages}
+    # return flask.render_template('home/index.html', packages=test_packages)
 
 
-@app.route('/about.html')
+@app.route('/about')
+@response(template_file='home/about.html')
 def about():
-    return flask.render_template('about.html')
+    return {}
 
 
 if __name__ == '__main__':
